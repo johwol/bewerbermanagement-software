5af800d49de971474df1da0f6b5ecbcd4d9feda0
Johannes <>
2023-02-12 15:01:27 +0100

    add basePath and assetPrefix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: "/bewerbermanagement-software",
+  assetPrefix: "/bewerbermanagement-software",
 }
 
 module.exports = nextConfig
